b03d4b5b92d84a529c5be435cac57d81e52ee24f
Kenvi <604113530@qq.com>
2017-01-12 17:21:38 +0800

    *

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <winter></winter>
+  </div>
+</template>
+
+<script>
+import Winter from './components/Winter'
+
+export default {
+  name: 'app',
+  components: {
+    Winter
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  img {
+    height: auto;
+    width: auto;
+  }
+}
+</style>
---
+++ src/components/Winter.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  .stars-wrapper#scene
+    .layer(data-depth="0.00")
+    .stars-cluster.stars-cluster-1.layer(data-depth="0.30")
+      - for (var x = 1; x <=40; x++)
+        .star(class="star-"+x)
+    .stars-cluster.stars-cluster-2.layer(data-depth="0.50")
+      - for (var x = 1; x <=40; x++)
+        .star(class="star-"+x)
+    .stars-cluster.stars-cluster-3.layer(data-depth="0.90")
+      - for (var x = 1; x <=15; x++)
+        .star(class="star-"+x)
+    .moon-wrapper.layer(data-depth="0.30")
+      .moon
+    .title-wrapper.layer(data-depth="0.40")
+      h1.title(data-title="Fly me to the moon") Fly me to the moon
+</template>
+<style lang="less">
+  @import "../assets/less/winter.less";
+  @import "https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css";
+</style>
+<script>
+  import $ from 'jquery'
+  require("imports?$=jquery!../assets/js/jquery.parallax.js")
+  $(function() {
+    $('#scene').parallax({
+      calibrateX: true,
+      calibrateY: true,
+      scalarX: 12,
+      scalarY: 12,
+      frictionX: 0.1,
+      frictionY: 0.1,
+      originX: 0.5,
+      originY: 0.5
+    });
+  });
+
+    export default{
+        name:'Winter',
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        }
+    }
+</script>
